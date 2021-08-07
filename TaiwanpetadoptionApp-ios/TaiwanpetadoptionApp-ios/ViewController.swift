7a7e19983e0825a9290caf38d837bb571817a839
wei0820 <jheng.wei0919@gmail.com>
2021-08-07 22:53:55 +0800

    add firebase add almofire

--- TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/ViewController.swift
+++ TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getData()
     }
 
 
+    
+    func getData(){
+        AF.request("https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL").responseJSON { response in
+            print("Response:")
+
+            print("Response: \(response)")
+        }
+    }
 }
 
