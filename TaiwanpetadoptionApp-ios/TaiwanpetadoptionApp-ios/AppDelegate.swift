eb867f23e681c5daaaf25b73b13177cd865efd28
Jack Pan <jackpan@heyu.tw>
2021-08-11 18:18:17 +0800

    add vpon sdk

--- TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/AppDelegate.swift
+++ TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/AppDelegate.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 import Firebase
-@main
+import GoogleMobileAds
+import VpadnSDKAdKit
+
+
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        let config = VpadnAdConfiguration.sharedInstance()
+               config.logLevel = .debug
+               config.initializeSdk()
         return true
     }
 
