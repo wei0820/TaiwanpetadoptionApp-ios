6130588e3357a23de94ccbdda44db007d6e9245e
Jack Pan <jackpan@heyu.tw>
2021-11-16 15:38:57 +0800

    add view

---
+++ TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/ShareViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ShareViewController.swift
+//  TaiwanpetadoptionApp-ios
+//
+//  Created by mac on 2021/11/16.
+//
+
+import UIKit
+import Alamofire
+import AppTrackingTransparency
+import AdSupport
+import GoogleMobileAds
+import JGProgressHUD
+import JJFloatingActionButton
+
+class ShareViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
+    
+    
+    var myIndex : IndexPath = IndexPath()
+    var arrayData :[Data] = [Data]()
+    @IBOutlet weak var tableView: UITableView!
+    struct CellIdentifier {
+        static let identifier = "dataCell"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayData.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.identifier, for: indexPath) as? DataTableViewCell else {
+              
+              
+              return UITableViewCell()
+          }
+        
+
+
+      
+             return cell
+         }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .light
+        } else {
+            
+                       // Fallback on earlier versions
+        }
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        setActionButton()
+        
+    
+    }
+    
+    
+    func  setActionButton() -> Void{
+         
+         let actionButton = JJFloatingActionButton()
+         
+         actionButton.addItem(title: "新增", image: UIImage(named: "create")?.withRenderingMode(.alwaysTemplate)) { item in
+         }
+         
+         
+         actionButton.addItem(title: "會員資訊", image: UIImage(named: "money_black")?.withRenderingMode(.alwaysTemplate)) { item in
+     
+             
+        }
+         
+         actionButton.addItem(title: "發文規範", image: UIImage(named: "report_black")?.withRenderingMode(.alwaysTemplate)) { item in
+         let controller = UIAlertController(title: "發文規範", message:"本討論區只提供分享,請勿發表帶風向,人身攻擊,招收會員,招收群組等 違規的文章 如發現違規文章 將刪除該文章", preferredStyle: .alert)
+           let okAction = UIAlertAction(title: "好的", style: .default,handler: nil)
+                     controller.addAction(okAction)
+           self.present(controller, animated: true, completion: nil)
+             // do something
+         }
+         
+         view.addSubview(actionButton)
+         actionButton.itemAnimationConfiguration = .circularSlideIn(withRadius: 125)
+         actionButton.buttonAnimationConfiguration = .rotation(toAngle: .pi * 3 / 3)
+         actionButton.buttonAnimationConfiguration.opening.duration = 0.8
+         actionButton.buttonAnimationConfiguration.closing.duration = 0.6
+         
+         actionButton.translatesAutoresizingMaskIntoConstraints = false
+         actionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26).isActive = true
+         actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -56).isActive = true
+     }
+     
+    
+
+}
