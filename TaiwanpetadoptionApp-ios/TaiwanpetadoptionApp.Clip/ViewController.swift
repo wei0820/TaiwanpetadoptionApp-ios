d040d0be6bb80900b40b0a4ceb19cfae63072d2a
Jack Pan <jackpan@heyu.tw>
2021-11-16 10:11:30 +0800

    add libs

---
+++ TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp.Clip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaiwanpetadoptionApp.Clip
+//
+//  Created by mac on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
