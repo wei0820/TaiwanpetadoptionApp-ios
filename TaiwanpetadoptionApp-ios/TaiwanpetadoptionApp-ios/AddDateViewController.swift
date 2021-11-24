a798af7174e8da632b924291b2c9a8eb80353b5f
Jack Pan <jackpan@heyu.tw>
2021-11-24 13:49:36 +0800

    新增 手機 正規表示

--- TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/AddDateViewController.swift
+++ TaiwanpetadoptionApp-ios/TaiwanpetadoptionApp-ios/AddDateViewController.swift
@@ -410,6 +410,19 @@ extension AddDateViewController: UIImagePickerControllerDelegate, UINavigationCo
          
      }
     
-    
+    //驗證手機號
+    class func isPhoneNumber(phoneNumber:String) -> Bool {
+        if phoneNumber.count == 0 {
+            return false
+        }
+        let mobile = "^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$"
+        let regexMobile = NSPredicate(format: "SELF MATCHES %@",mobile)
+        if regexMobile.evaluate(with: phoneNumber) == true {
+            return true
+        }else
+        {
+            return false
+        }
+    }
     
 }
